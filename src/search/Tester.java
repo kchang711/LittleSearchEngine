0914b661cd55c061f357ddf80165f863c08e2e43
kchang711 <kchang711@yahoo.com>
2017-11-21 18:54:09 -0500

    Search Engine

---
+++ src/search/Tester.java
@@ -0,0 +1,11 @@
+package search;
+
+public class Tester {
+
+	public static void main(String[] arg){
+		LittleSearchEngine t1 = new LittleSearchEngine();
+		System.out.println(t1.getKeyWord("herro'dfd"));
+		
+	}
+	
+}
